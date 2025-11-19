b3cbaac91a1285de2e546ed518fcae26f55f892c
Cybereign <31578805+Cybereign@users.noreply.github.com>
2025-11-19 17:10:04 +0800

    Update default-data.js

--- default-data.js
+++ default-data.js
@@ -12,6 +12,15 @@ const DEFAULT_TABLE_DATA = [
     },
         {
         "id": 2,
+        "category": "整车配置",
+        "title": "DH3整车配置清单",
+        "path": "https://acndoaymjsa1.feishu.cn/sheets/M7prsqZR7hcuzbt0zZtcDBTmnjf?from=from_copylink&sheet=1sERLJ",
+        "type": "url",
+        "note": "【飞书】DH3整车配置表\n11月14日版本"
+    },
+    
+        {
+        "id": 3,
         "category": "联系信息",
         "title": "DH项目整体联系人清单",
         "path": "https://acndoaymjsa1.feishu.cn/sheets/Gbh8sTDmdhMD9DtZcC2c7trunRe",
@@ -19,7 +28,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【飞书】包含DH1，DH3各业务板块联系人信息"
     },
     {
-        "id": 3,
+        "id": 4,
         "category": "设计评审",
         "title": "DH3电子电气拓扑图",
         "path": "w:\\yling",
@@ -27,7 +36,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【内网】w:\\yling\n请确保你的账号已经切换到内网"
     },
     {
-        "id": 4,
+        "id": 5,
         "category": "阶段评审",
         "title": "DH3项目J2阶段评审材料输入信息",
         "path": "\\\\10.4.9.25\\Project\\DH系列\\060-整车开发\\DH3\\J2评审输入",
@@ -35,7 +44,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【外网】J2阶段输入物数据库"
     },
     {
-        "id": 5,
+        "id": 6,
         "category": "阶段评审",
         "title": "DH3项目J2阶段评审材料归档路径",
         "path": "\\\\10.4.9.25\\Project\\DH系列\\130-智能软件\\DH3\\130100_ 阶段评审材料\\130102_ J2阶段评审材料及会议纪要",
@@ -43,7 +52,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【外网】请大家把J2材料及会议纪要放入此文件夹"
     },
     {
-        "id": 6,
+        "id": 7,
         "category": "开发计划",
         "title": "DH3项目智软控制器开发计划",
         "path": "https://acndoaymjsa1.feishu.cn/file/K8KlbxqKmoZV2HxoG94ccLotnne",
@@ -51,7 +60,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【飞书】请大家在此更新各自开发计划"
     },
     {
-        "id": 7,
+        "id": 8,
         "category": "联系信息",
         "title": "DH3项目华为接口联系人清单",
         "path": "https://acndoaymjsa1.feishu.cn/file/NI95bmHjpoopKBxVGhrc7S9SnUc",
@@ -59,7 +68,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【飞书】包含华为座舱、智驾、网联相关接口人信息"
     },
     {
-        "id": 8,
+        "id": 9,
         "category": "联系信息",
         "title": "DH3项目研发总院智软内部责任分工",
         "path": "https://acndoaymjsa1.feishu.cn/wiki/YyMcwejHNifAkfk0aEXcjdeQnTb?table=tbltH0YNXCFoOxNm&view=vewS54CcOf",
@@ -67,7 +76,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【飞书】仅包含东风研发总院智软内部相关责任人分工，仅智软群成员可访问"
     },
     {
-        "id": 9,
+        "id": 10,
         "category": "再发防止",
         "title": "DH3不满再发防止-整车级",
         "path": "https://acndoaymjsa1.feishu.cn/file/BzEsbuCLCowqs2xT1y0cRe1JnSb",
@@ -75,7 +84,7 @@ const DEFAULT_TABLE_DATA = [
         "note": "【飞书】DH3不满再发防止-整车级"
     },
     {
-        "id": 10,
+        "id": 11,
         "category": "再发防止",
         "title": "DH3 LLR和PRC再发防止 - 总院级",
         "path": "https://doc.weixin.qq.com/sheet/e3_AKkAcQbnAKcCNoUhJadOcRr27t3Mb?scode=AGwAPQeYAA0i5kLUWBAbgALQaUAJ8&version=5.0.0.8619&platform=win&tab=mfripw",
@@ -90,6 +99,7 @@ const DEFAULT_CATEGORY_LIST = [
     { id: "category3", name: "阶段评审" },
     { id: "category4", name: "联系信息" },
     { id: "category5", name: "再发防止" },
+    { id: "category6", name: "整车配置" },
     { id: "category6", name: "其他" }
 ];
 
@@ -99,6 +109,7 @@ const DEFAULT_CATEGORY_MAP = {
     "category3": "阶段评审",
     "category4": "联系信息",
     "category5": "再发防止",
+    "category6": "整车配置",
     "category6": "其他"
 };
 
